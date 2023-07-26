f86c67abacb92da38be476c555159f3b932394f2
NuocDua <dungga7541000@gmail.com>
2023-07-26 14:15:47 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Home from "./pages/container/Home/Home"
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
